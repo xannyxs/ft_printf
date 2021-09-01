23af8a4d0223fea515c71cc16076e38f5fcf90b5
xvoorvaa <xvoorvaa@codam.student.nl>
2021-09-01 15:25:25 +0200

    Norminette fix

--- mandatory/src/print/print_s.c
+++ mandatory/src/print/print_s.c
@@ -6,7 +6,7 @@
 /*   By: xvoorvaa <xvoorvaa@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/06/07 15:57:00 by xvoorvaa      #+#    #+#                 */
-/*   Updated: 2021/07/12 13:02:34 by xander        ########   odam.nl         */
+/*   Updated: 2021/09/01 15:21:02 by xander        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	print_s(va_list ap)
 {
 	char	*ptr;
-	int 	len;
+	int		len;
 
 	len = 0;
 	ptr = va_arg(ap, char *);
